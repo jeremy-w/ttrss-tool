3845bf94520949d0e3f6e390471ebbe1d9d954b3
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2013-08-01 01:36:17 -0400

    adds start of ttrss-tool

---
+++ ttrss-tool.go
@@ -0,0 +1,58 @@
+// vi: set noet ts=4 sw=4 ft=go tw=79:
+/* tt manipulates tiny-tiny-rss subscriptions. */
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+)
+var _ = http.StatusContinue
+
+// Exit Codes
+const (
+	EX_USAGE = 64
+)
+
+// General Flags
+var (
+	flagAddr string
+	flagUser string
+	flagPass string
+)
+
+type Cmd struct {
+	Flag flag.FlagSet
+	Run func(cmd *Cmd, args []string)
+}
+
+func runList(cmd *Cmd, args []string) {
+	fmt.Println("RUNNING LIST")
+}
+
+var cmds = map[string]Cmd {
+	"ls": {flag.FlagSet{}, runList},
+}
+
+func init() {
+	flag.StringVar(&flagAddr, "addr", "",
+		"address (example: https://example.com/tt-rss/)")
+	flag.StringVar(&flagUser, "user", "admin", "user to connect as")
+	flag.StringVar(&flagPass, "pass", "password", "password to use")
+
+	for k, v := range cmds {
+		v.Flag.Init(k, flag.PanicOnError)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(EX_USAGE)
+	}
+
+	fmt.Println(flagAddr, flagUser, flagPass, flag.Args())
+}
