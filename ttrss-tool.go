ca3b1169f41732a22c3a1435401d06b33d235583
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2013-08-03 23:25:24 -0400

    extracts TTRSSClient and its methods Call, Login
    Ln.Run now issues subscribeToFeed call, but does not yet parse out the
    response.

--- ttrss-tool.go
+++ ttrss-tool.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -145,6 +146,98 @@ func (ln *Ln) Synopsis(w io.Writer) {
 	fmt.Println("ln feed [catpath] -- subscribes to a new feed")
 }
 
+type TTRSSClient struct {
+	ApiEP string
+	Client http.Client
+	SessionID string
+}
+
+// TTRSSResp represents the JSON response returned by the TTRSS API.
+type TTRSSResp struct {
+	// Same as request "seq" number, if provided.
+	// Otherwise mostly 0, but sometimes null.
+	Seq int
+
+	// TTRSS_API_STATUS_* value (hopefully)
+	Status int
+
+	// Content["error"] wrapped as an error; nil if not present or not string
+	Error error
+
+	// Content of the response.
+	Content map[string]interface{}
+}
+
+// Call issues an API request.
+// If an error status is returned, tt.Error will be set.
+// If an HTTP connection error occurs, the application terminates with an
+// error message, and the call does not return.
+func (tt *TTRSSClient) Call(op string, body map[string]interface{}) (resp TTRSSResp) {
+	body["op"] = op
+	if len(tt.SessionID) > 0 {
+		body["sid"] = tt.SessionID
+	}
+	fmt.Println("issuing call:", body)
+
+	buffer := asJSONBuffer(body)
+	httpResp, err := tt.Client.Post(tt.ApiEP, "application/json", &buffer)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "connection error: %v\n", err)
+		os.Exit(EX_DATAERR)
+	}
+
+	defer httpResp.Body.Close()
+	dec := json.NewDecoder(httpResp.Body)
+	err = dec.Decode(&resp)
+	if err != nil {
+		fmt.Fprintf(os.Stderr,
+			"API JSON response was malformed: %v - "+
+			"are you sure you supplied the correct URL?\n", err)
+		os.Exit(EX_PROTOCOL)
+	}
+
+	resp.Error = nil
+	if apiError, ok := resp.Content["error"]; ok {
+		if errorString, ok := apiError.(string); ok {
+			resp.Error = errors.New(errorString)
+		}
+	}
+	if resp.Status != API_STATUS_OK && resp.Error == nil {
+		resp.Error = errors.New("(response contained no error text)")
+	}
+	return
+}
+
+// Logs into the host as the designated user.
+// Terminates the program with an error message if login fails.
+// Otherwise, updates tt.ApiEP and tt.SessionID.
+func (tt *TTRSSClient) Login(hostURL string, user string, password string) {
+	apiEP := hostURL
+	if !strings.HasSuffix(apiEP, "/") {
+		apiEP += "/"
+	}
+	apiEP += "api/"
+	tt.ApiEP = apiEP
+	fmt.Println("trying to log in as", user)
+
+	loginMap := map[string]interface{} {
+		"user": user,
+		"password": password,
+	}
+	resp := tt.Call("login", loginMap)
+
+	sessionID, ok := resp.Content["session_id"]
+	if !ok || resp.Status != API_STATUS_OK {
+		msg := "error: failed to log in at %s as %s"
+		if resp.Error != nil {
+			msg += ": " + resp.Error.Error()
+		}
+		log.Fatalf(msg, apiEP, flUser)
+	}
+	tt.SessionID = sessionID.(string)
+	fmt.Println("logged in as", user, "with sessionID", tt.SessionID)
+}
+
 func (ln *Ln) Run(args []string) {
 	ln.flags.Parse(args)
 
@@ -162,50 +255,20 @@ func (ln *Ln) Run(args []string) {
 	feed := ln.flags.Arg(0)
 	catpath := ln.flags.Arg(1)
 
-	loginMap := map[string]string {
-		"op": "login",
-		"user": flUser,
-		"password": flagPass,
-	}
-	loginBuffer := asJSONBuffer(loginMap)
+	var tt TTRSSClient
+	tt.Login(flAddr, flUser, flPass)
 
-	apiEP := flAddr
+	subscribeMap := map[string]interface{} {
+		"feed_url": feed,
+		//"category_id": catID  // int - defaults to 0 aka Uncategorized
+		//"login": ln.flLogin  // if required
+		//"password": ln.flPassword // if required
 	}
-	apiEP += "api/"
-
-	var client http.Client
-	httpResp, err := client.Post(apiEP, "application/json", &loginBuffer)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "connection error: %v\n", err)
-		os.Exit(EX_DATAERR)
+	resp := tt.Call("subscribeToFeed", subscribeMap)
+	if resp.Error != nil {
+		log.Fatalf("error: unable to link feed %s at catpath %s: %s",
+			feed, catpath, resp.Error)
 	}
-
-	defer httpResp.Body.Close()
-	type TTRSSResp struct {
-		Seq int
-		Status int
-		Content map[string]interface{}
-	}
-	var resp TTRSSResp
-	dec := json.NewDecoder(httpResp.Body)
-	err = dec.Decode(&resp)
-	if err != nil {
-		fmt.Fprintf(os.Stderr,
-			"API JSON response was malformed: %v - "+
-			"are you sure you supplied the correct URL?\n", err)
-		os.Exit(EX_PROTOCOL)
-	}
-	sessionID, ok := resp.Content["session_id"]
-	if !ok || resp.Status != API_STATUS_OK {
-		msg := "error: failed to log in at %s as %s"
-		if apiError, ok := resp.Content["error"]; ok {
-			if errorText, ok := apiError.(string); ok {
-				msg += ": " + errorText
-			}
-		}
-		log.Fatalf(msg, apiEP, flUser)
-	}
-	fmt.Println("sessionID", sessionID, feed, catpath)
 }
 
 type Ls struct {
